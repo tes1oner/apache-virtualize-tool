592709611b2336f89a264ef830d52b43cddc1461
Elías Peraza <eliasperaza0511@gmail.com>
2016-12-02 10:38:41 -0400

    .

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env sh
-cd ..
+cd "../"
 echo "Installing..."
-cp apache-virtualize-tools /etc/apache-virtualize-tools
+cp -a apache-virtualize-tool /etc/apache-virtualize-tools
 echo "Linking easy access"
-ln -a /etc/apache-virtualize-tools/avh.py /usr/local/bin/avh
+ln /etc/apache-virtualize-tools/avh.py /usr/local/bin/avh
